72495e5b6819b8d0429c6fbf239f5b85f02f1b36
Harika Ogirala <70664573+HarikaOgirala@users.noreply.github.com>
2020-10-11 17:41:03 -0400

    Delete InsufficientFundsException.java
    package structure implemented and moved the classes to appropriate package

--- Assignment2/src/com/designpatterns/assignment2/InsufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package com.designpatterns.assignment2;
-
-public class InsufficientFundsException extends Exception {
-
-	 public InsufficientFundsException(String errorMessage) {
-	        super(errorMessage);
-}
-}
